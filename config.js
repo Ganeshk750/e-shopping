e3dc8b076255f2d922ae9bf64901d1515b8bef91
Ganeshk750 <ganesh9ot@gmail.com>
2019-11-19 14:05:30 +0530

    basic server and database setups

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database :'mongodb://localhost:27017/amazono',
+    port: process.env.PORT || 3000,
+    secret: 'ganesh123'
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const config = require('./config');
+
+mongoose.connect(config.database,{ useUnifiedTopology: true, useNewUrlParser: true }, err =>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Connected to the database');
+    }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors());
+
+app.get('/', (req,res, next) =>{
+    res.json({
+        user: 'Ganesh'
+    })
+});
+
+app.listen(config.port, err =>{
+    console.log('Server is running on port'+ config.port);
+});
