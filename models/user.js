fa30614a91f0068039fc395c417ed252aa73d418
Ganeshk750 <ganesh9ot@gmail.com>
2019-11-23 20:38:56 +0530

    ading user models

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+ email:{
+     type: String,
+     unique: true,
+     lowercase: true
+    },
+   name: String,
+   password: String,
+   picture: String,
+   isSeller: { 
+       type: Boolean,
+       default: false
+    },
+ address:{
+     addr1: String,
+     addr2: String,
+     city: String,
+     state: String,
+     country: String,
+     postalCode: String
+ },
+ created: { type: Data, default: Date.now}
+});
